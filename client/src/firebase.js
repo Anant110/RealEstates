82da988d7083ff3ccae7193ac2257d6bbf5edd52
Anant Pratap Singh <anant.abes459@gmail.com>
2023-12-08 17:45:06 +0530

    first commit

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBsNYyy3ErWUdaRzLk-UfIvVqTwLhY_NmY",
+  authDomain: "realtor-51bf6.firebaseapp.com",
+  projectId: "realtor-51bf6",
+  storageBucket: "realtor-51bf6.appspot.com",
+  messagingSenderId: "1045551419229",
+  appId: "1:1045551419229:web:56d4f788765e4fb58c22b4"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
